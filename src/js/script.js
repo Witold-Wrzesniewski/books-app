b95af26ebeb981108fc645fbbd538f2d9add3b9e
Witold Wrześniewski <witold.wrzesniewski@poczta.fm>
2025-06-04 18:00:16 +0200

    Add generateBooksList function

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,10 @@
-const generateBookList = function(){
-  
+const generateBooksList = function(){
+  const template = Handlebars.compile(document.querySelector('#template-book').innerHTML);
+  const booksList = document.querySelector('.books-list');
+  for(const book of dataSource.books){
+    const generatedHTML = template(book);
+    const element = utils.createDOMFromHTML(generatedHTML);
+    booksList.appendChild(element);
+  }
 };
+generateBooksList();
