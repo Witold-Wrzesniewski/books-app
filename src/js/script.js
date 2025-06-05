52f742a34890cc1213b7408d6fefc5f2bd882f37
Witold Wrześniewski <witold.wrzesniewski@poczta.fm>
2025-06-05 19:31:57 +0200

    Add initActions function

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-const generateBooksList = function(){
+const render = function(){
   const template = Handlebars.compile(document.querySelector('#template-book').innerHTML);
   const booksList = document.querySelector('.books-list');
   for(const book of dataSource.books){
@@ -7,4 +7,24 @@ const generateBooksList = function(){
     booksList.appendChild(element);
   }
 };
-generateBooksList();
+render();
+
+const favoriteBooks = [];
+const initActions = function(){
+  const aElementsArr = document.querySelectorAll('.books-list .book__image');
+  for(let aElem of aElementsArr){
+    aElem.addEventListener('dblclick', function(event){
+      event.preventDefault();
+      if(!this.classList.contains('favorite')){
+        favoriteBooks.push(this.getAttribute('data-id'));
+        this.classList.add('favorite');
+      } else{
+        const index = favoriteBooks.indexOf('favorite');
+        favoriteBooks.splice(index, 1);
+        this.classList.remove('favorite');
+      }
+      console.log(favoriteBooks);
+    });
+  }
+}
+initActions();
