e0aea474d38e18d577d345e810f9b0dd31b2ca18
Witold Wrześniewski <witold.wrzesniewski@poczta.fm>
2025-06-07 17:49:01 +0200

    Add filterBooks function

--- src/js/script.js
+++ src/js/script.js
@@ -10,45 +10,71 @@ const render = function(){
 render();
 
 const favoriteBooks = [];
+const filters = [];
 
-/*const initActions = function(){
-  const aElementsArr = document.querySelectorAll('.books-list .book__image');
-  for(let aElem of aElementsArr){
-    aElem.addEventListener('dblclick', function(event){
-      event.preventDefault();
-      if(!this.classList.contains('favorite')){
-        favoriteBooks.push(this.getAttribute('data-id'));
-        this.classList.add('favorite');
-      } else{
-        const index = favoriteBooks.indexOf(this.getAttribute('data-id'));
-        favoriteBooks.splice(index, 1);
-        this.classList.remove('favorite');
+const filterBooks = function(){
+
+  const renderFilter = function(elemId, action){
+    const booksLinkArr = document.querySelector('.books-list').querySelectorAll('.book__image');
+    for(const bookLink of booksLinkArr){
+      if(bookLink.getAttribute('data-id') == elemId){
+        if(action === 'check')
+          bookLink.classList.add('hidden');
+        else if(action === 'uncheck')
+          bookLink.classList.remove('hidden');
       }
-      console.log(favoriteBooks);
-    });
+    }
+  };
+
+  for(const book of dataSource.books){
+    for(const detail in book.details){
+      const detailVal = book.details[detail];
+      if(detailVal && filters.indexOf(detail) !== -1){
+        renderFilter(book.id, 'check');
+      }else if(detailVal && filters.indexOf(detail) === -1){
+        renderFilter(book.id, 'uncheck');
+      }
+    }
   }
-};*/
+}
+
 const initActions = function(){
   const booksListElem = document.querySelector('.books-list');
   
-    booksListElem.addEventListener('dblclick', function(event){
-      event.preventDefault();
-      const linkElem = event.target.offsetParent;
-      if(linkElem.classList.contains('book__image')){
-        if(!linkElem.classList.contains('favorite')){
-          favoriteBooks.push(linkElem.getAttribute('data-id'));
-          linkElem.classList.add('favorite');
-        } else{
-          const index = favoriteBooks.indexOf(linkElem.getAttribute('data-id'));
-          favoriteBooks.splice(index, 1);
-          linkElem.classList.remove('favorite');
-        }
+  booksListElem.addEventListener('dblclick', function(event){
+    event.preventDefault();
+    const linkElem = event.target.offsetParent;
+    if(linkElem.classList.contains('book__image')){
+      if(!linkElem.classList.contains('favorite')){
+        favoriteBooks.push(linkElem.getAttribute('data-id'));
+        linkElem.classList.add('favorite');
+      } else{
+        const index = favoriteBooks.indexOf(linkElem.getAttribute('data-id'));
+        favoriteBooks.splice(index, 1);
+        linkElem.classList.remove('favorite');
       }
-      
-      console.log(favoriteBooks);
-    });
-    booksListElem.addEventListener('click', function(event){
-      event.preventDefault();
-    });
+    }
+    console.log(favoriteBooks);
+  });
+  booksListElem.addEventListener('click', function(event){
+    event.preventDefault();
+    //console.log(event.detail);
+  });
+
+  const filtersElem = document.querySelector('.filters');
+  filtersElem.addEventListener('click', function(event){
+    const clickedElem = event.target;
+    if(clickedElem.tagName == 'INPUT'){
+      if(clickedElem.checked){
+        filters.push(clickedElem.value);
+      } else{
+        const index = filters.indexOf(clickedElem.value);
+        filters.splice(index, 1);
+      }
+
+      console.log(filters);
+      filterBooks();
+    }
+  });
 }
 initActions();
